750d3a58649b47775a1471e5b70fff5132e961d5
TaiLowRen <farawayplanet9090@gmail.com>
2022-05-31 18:43:59 -0500

    test

---
+++ returnNegatives.js
@@ -0,0 +1,10 @@
+function makeNegative(num) {
+    //check if number is negative 
+      if(num >= 0){
+        return (0 - num)
+      } else {
+        return num
+      }
+    }
+
+//8
---
+++ reversedStrings.js
@@ -0,0 +1,8 @@
+function solution(str){
+    spstr = str.split("")
+     revstr= spstr.reverse()
+    joinstr = revstr.join("")
+    return joinstr;
+  }
+
+  //8
